996f6dd4b60af07b8fab30ac43de98eb5e441db1
August Hovland <gushov@gmail.com>
2015-08-15 00:48:31 +0200

    Fixed missing header file

--- LILKit/LILKit.h
+++ LILKit/LILKit.h
@@ -18,4 +18,4 @@ FOUNDATION_EXPORT const unsigned char LILKitVersionString[];
 #import <LILKit/LILLogFormatter.h>
 #import <LILKit/NSString+LILAdditions.h>
 #import <LILKit/LILUTCDateTransformer.h>
-#import <LILKit/LILUTCDateTransformer.h>
+#import <LILKit/LILRFC3339DateTransformer.h>
